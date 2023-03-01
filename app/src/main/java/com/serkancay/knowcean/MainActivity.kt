7f5589961b86b4bf0928d9f1c64d3a68b7e4bd71
Serkan Cay <sserkancay@gmail.com>
2023-03-01 21:59:53 +0300

    Recreate project using Jetpack Compose

--- app/src/main/java/com/serkancay/knowcean/MainActivity.kt
+++ app/src/main/java/com/serkancay/knowcean/MainActivity.kt
@@ -1,11 +1,43 @@
 package com.serkancay.knowcean
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.serkancay.knowcean.ui.theme.KnowceanTheme
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            KnowceanTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    KnowceanTheme {
+        Greeting("Android")
     }
 }
