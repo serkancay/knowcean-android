2ac27271d790000b4b1f36e86b12a4f37fcd736a
Serkan Cay <sserkancay@gmail.com>
2023-03-02 23:16:44 +0300

    Add body UI

--- app/src/main/java/com/serkancay/knowcean/Dummy.kt
+++ app/src/main/java/com/serkancay/knowcean/Dummy.kt
@@ -3,4 +3,10 @@ package com.serkancay.knowcean
 object Dummy {
     const val IMAGE_URL = "https://picsum.photos/800/600"
     const val TITLE = "Sinuhe'nin Hikayesi"
+    const val BODY = "Sinuhe'nin Hikayesi, Eski Mısır edebiyatına ait başlıksız, Orta Krallığın 12. hanedanı dönemine tarihlenen bir metindir. Yazarı bilinmeyen metinde ben-anlatımı kullanılarak muhtemelen kurgusal bir karakter olan saray memuru Sinuhe'nin hayat hikayesi aktarılır."
+    const val BODY_LARGE = "Sinuhe'nin Hikayesi, Eski Mısır edebiyatına ait başlıksız, Orta Krallığın 12. hanedanı dönemine tarihlenen bir metindir. Yazarı bilinmeyen metinde ben-anlatımı kullanılarak muhtemelen kurgusal bir karakter olan saray memuru Sinuhe'nin hayat hikayesi aktarılır. Anlatıma göre Sinuhe Firavun I. Amenemhat'ın ölümü üzerine paniğe kapılır ve macera dolu bir yolculukla ülkesinden kaçmaya başlar. Yolculuğunun sonunda Filistin bölgesine yerleşir. Ancak yaşlandıkça vatanını özlemektedir. Nihayet I. Senusret ona vatanına geri dönmesi için davette bulunur. I. Senusret Sinuhe'nin babası Firavun Amenemhet'in ölümünde bir rolü olmadığının farkına varmıştır. Bunun üzerine ülkesine geri dönen Sinuhe orada büyük bir onurla karşılanır.\n" +
+            "\n" +
+            "Anlatım yoğun bir biçimde I. Senusret'in sarayından bahseder ve bu nedenle bir çeşit propaganda olarak değerlendirilir. \n" +
+            "\n" +
+            "Birçok mısırbilimci Sinuhe'nin hikayesinin Eski Mısır edebiyatına ait en tanındık ve en kaliteli metin olduğu konusunda hemfikirdir.[1][2][3] Ortaya çıktığı dönemden sonra dahi okunmaya devam eden Sinuhe'nin hikayesi modern dönemde edebiyat ve film sektörü tarafından da ele alındı."
 }
--- app/src/main/java/com/serkancay/knowcean/MainActivity.kt
+++ app/src/main/java/com/serkancay/knowcean/MainActivity.kt
@@ -3,8 +3,12 @@ package com.serkancay.knowcean
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -14,6 +18,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
@@ -29,13 +34,33 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Column(modifier = Modifier.fillMaxSize()) {
-                        Header()
-                    }
+                    ArticleScreen()
                 }
             }
         }
     }
+}
+
+@Composable
+fun ArticleScreen() {
+    Column(modifier = Modifier.fillMaxSize()) {
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .verticalScroll(rememberScrollState())
+        ) {
+            Header()
+            Body()
+        }
+        Button(
+            onClick = { /*TODO*/ }, modifier = Modifier
+                .align(Alignment.End)
+                .padding(16.dp)
+        ) {
+            Text(text = stringResource(id = R.string.next_button))
+        }
+    }
+
 }
 
 @Composable
@@ -75,6 +100,11 @@ fun Header(
                 .padding(16.dp)
         )
     }
+}
+
+@Composable
+fun Body(body: String = Dummy.BODY_LARGE, modifier: Modifier = Modifier) {
+    Text(text = body, modifier = modifier.padding(16.dp))
 }
 
 @Preview(showBackground = true)
