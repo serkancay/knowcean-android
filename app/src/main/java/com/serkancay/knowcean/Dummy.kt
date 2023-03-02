b65262f146dfa506315c8647bb22e27746027a90
Serkan Cay <sserkancay@gmail.com>
2023-03-02 22:57:59 +0300

    Add header UI

---
+++ app/src/main/java/com/serkancay/knowcean/Dummy.kt
@@ -0,0 +1,6 @@
+package com.serkancay.knowcean
+
+object Dummy {
+    const val IMAGE_URL = "https://picsum.photos/800/600"
+    const val TITLE = "Sinuhe'nin Hikayesi"
+}
--- app/src/main/java/com/serkancay/knowcean/MainActivity.kt
+++ app/src/main/java/com/serkancay/knowcean/MainActivity.kt
@@ -3,13 +3,20 @@ package com.serkancay.knowcean
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 import com.serkancay.knowcean.ui.theme.KnowceanTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +29,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Column(modifier = Modifier.fillMaxSize()) {
+                        Header()
+                    }
                 }
             }
         }
@@ -30,14 +39,48 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Header(
+    title: String = Dummy.TITLE,
+    imageUrl: String = Dummy.IMAGE_URL,
+    modifier: Modifier = Modifier
+) {
+    Box(
+        modifier = modifier
+            .height(250.dp)
+    ) {
+        AsyncImage(
+            model = imageUrl,
+            contentDescription = title,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(
+                    brush = Brush.verticalGradient(
+                        colors = listOf(
+                            Color.Transparent,
+                            MaterialTheme.colorScheme.primary
+                        )
+                    )
+                )
+        )
+        Text(
+            text = title,
+            color = Color.White,
+            style = MaterialTheme.typography.titleLarge,
+            modifier = Modifier
+                .align(Alignment.BottomStart)
+                .padding(16.dp)
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     KnowceanTheme {
-        Greeting("Android")
+        Header()
     }
 }
